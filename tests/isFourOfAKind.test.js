4baa657cdb4de47b0d6bb567e005c44ad5346661
Swakshar Dutta <tarzan0607@gmail.com>
2023-04-19 14:27:14 +0200

    a changes in fourofkind

--- tests/isFourOfAKind.test.js
+++ tests/isFourOfAKind.test.js
@@ -8,6 +8,6 @@ test('Test that FourOfAKind returns truthy if four of a kind', () => {
 });
 
 test('Test that FourOfAKind returns falsy if not four of a kind', () => {
-  let hand = new Hand('♥7', '♦7', '♣3', '♠3', '♠7');
+  let hand = new Hand('♥9', '♦7', '♣3', '♠3', '♠7');
   expect(CompareHands.isFourOfAKind(hand)).toBeFalsy();
 });
