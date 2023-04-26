bcb95af5f58173e94d3bbb03a30a950a06b8dbb4
Swakshar Dutta <tarzan0607@gmail.com>
2023-04-26 14:36:22 +0200

    edited test files

--- tests/isFourOfAKind.test.js
+++ tests/isFourOfAKind.test.js
@@ -11,3 +11,16 @@ test('Test that FourOfAKind returns falsy if not four of a kind', () => {
   let hand = new Hand('♥9', '♦7', '♣3', '♠3', '♠7');
   expect(CompareHands.isFourOfAKind(hand)).toBeFalsy();
 });
+test('check that isFourOfAKind returns 0 if not four of a kind', () => {
+  let hand = new Hand('♣4', '♣5', '♥5', '♣5', '♣7');
+  expect(CompareHands.isFourOfAKind(hand)).toBe(0);
+});
+
+test('check that isFourOfAKind returns a highest score for the strongest hand', () => {
+  let hand1 = new Hand('♣5', '♣5', '♥5', '♣5', '♣7');
+  let hand2 = new Hand('♣8', '♣8', '♥8', '♣8', '♣4');
+  let score = CompareHands.isFourOfAKind(hand1);
+  let score2 = CompareHands.isFourOfAKind(hand2);
+  expect(score2).toBeGreaterThan(score)
+
+});
--- tests/isHighestCard.test.js
+++ tests/isHighestCard.test.js
@@ -6,3 +6,16 @@ test('Test that HighestCard returns truthy if there is  no other win condition l
   let hand = new Hand('♥A', '♦K', '♣J', '♠3', '♠7');
   expect(CompareHands.isHighestCard(hand)).toBeTruthy();
 });
+test('check that isHighestCard returns correct point value for compare hands', () => {
+  let hand1 = new Hand('♥3', '♦9', '♣7', '♠5', '♣6')
+  let hand2 = new Hand('♥3', '♦T', '♣7', '♠5', '♣6')
+  let scoreHand1 = CompareHands.isHighestCard(hand1);
+  let scoreHand2 = CompareHands.isHighestCard(hand2)
+  expect(scoreHand2).toBeGreaterThan(scoreHand1);
+});
+
+test('Test that isHighestCard returns highest score for strongest hand', () => {
+  let hand1 = new Hand('♥A', '♦K', '♣Q', '♠J', '♠T');
+  let hand2 = new Hand('♥A', '♦K', '♣Q', '♠J', '♠9');
+  expect(CompareHands.isHighestCard(hand1)).toBeGreaterThan(CompareHands.isHighestCard(hand2));
+});
