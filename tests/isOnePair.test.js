5cc4dc00f7ec2c88c3c4ec08e6cd0781027c0d87
Swakshar Dutta <tarzan0607@gmail.com>
2023-04-26 15:05:45 +0200

    Added more tests

--- tests/isOnePair.test.js
+++ tests/isOnePair.test.js
@@ -11,3 +11,11 @@ test('Test that OnePair returns falsy if no pair exists', () => {
   let hand = new Hand('♥A', '♦9', '♣J', '♠3', '♠7');
   expect(CompareHands.isOnePair(hand)).toBeFalsy();
 });
+
+test('check that isOnePair returns highest score for the strongest one pair hand', () => {
+  let hand1 = new Hand('♥9', '♦9', '♣7', '♠5', '♣6');
+  let hand2 = new Hand('♥Q', '♦Q', '♣7', '♠5', '♣6');
+  let scoreHand1 = CompareHands.isOnePair(hand1);
+  let scoreHand2 = CompareHands.isOnePair(hand2);
+  expect(scoreHand2).toBeGreaterThan(scoreHand1);
+});
